18bd09d8a810d3ba75f30a0b51607e416a928486
imcetron <imcetron.ti@gmail.com>
2022-06-21 13:03:56 -0500

    sdt

---
+++ src/components/Pages/sliderInfinito.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import m1 from '../../img/m-1.png';
+
+
+import './sliderInfinite.scss';
+
+
+export const SliderInfinite = () => {
+    return (
+    
+        <>
+		
+	<div className="cont-marcas">
+
+		<div className="rows">
+				<div className="row-1">
+				<div className="row">
+					<p>dsa</p>
+				</div>
+				<div className="row">
+				<p>dsa</p>
+				</div>
+				<div className="row">
+				<p>dsa</p>
+				</div>
+				<div className="row">
+				<p>dsa</p>
+				</div>
+				<div className="row">
+				<p>dsa</p>
+				</div>
+				<div className="row">
+				<p>dsa</p>
+				</div>
+				</div>
+				</div>
+		</div>
+
+
+
+
+
+
+
+</>
+
+)
+}
